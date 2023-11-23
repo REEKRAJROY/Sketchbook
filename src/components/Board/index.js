e0f513ebf2d0d1ac2960bdf23b63661801a93487
REEKRAJROY <reekrajroy@gmail.com>
2023-11-24 03:46:36 +0530

    Added socket connections

--- src/components/Board/index.js
+++ src/components/Board/index.js
@@ -2,6 +2,7 @@ import { MENU_ITEMS } from "@/constants";
 import { useEffect, useLayoutEffect, useRef } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { actionItemClick } from "@/slice/menuSlice";
+import { socket } from "@/socket";
 
 const Board = () => {
     const canvasRef = useRef(null)//did not understand
@@ -38,12 +39,21 @@ const Board = () => {
     const canvas = canvasRef.current;
     const context = canvas.getContext("2d");
 
-    const changeConfig = () => {
+    const changeConfig = (color, size) => {
         context.strokeStyle = color
         context.lineWidth = size
     }
 
-    changeConfig()
+    const handleChangeConfig = (config) => {
+        changeConfig(config.color, config.size)
+    }
+
+    changeConfig(color, size)
+    socket.on('changeConfig',handleChangeConfig)
+
+    return () => {
+        socket.off('changeConfig',handleChangeConfig)
+    }
     },[color,size])
 
     //this hook is used as there was an overlap between useEffect hooks. First this hook is called and consumed.
@@ -69,11 +79,13 @@ const Board = () => {
     const handleMouseDown = (e) => {
         shouldDraw.current = true
         beginPath(e.clientX, e.clientY)
+        socket.emit('beginPath', {x: e.clientX, y: e.clientY })
     }
 
     const handleMouseMove = (e) => {
         if(!shouldDraw.current) return
         drawLine(e.clientX, e.clientY)
+        socket.emit('drawLine', {x:e.clientX, y: e.clientY })
     }
     
     const handleMouseUp = (e) => {
@@ -82,15 +94,28 @@ const Board = () => {
         drawHistory.current.push(imageData)
         historyPointer.current = drawHistory.current.length - 1
     }
+    const handleBeginPath = (path) => {
+        beginPath(path.x, path.y)
+    }
+
+    const handleDrawLine = (path) => {
+        drawLine(path.x, path.y)
+    }
 
     canvas.addEventListener('mousedown', handleMouseDown)
     canvas.addEventListener('mousemove', handleMouseMove)
     canvas.addEventListener('mouseup', handleMouseUp)
 
+    socket.on("beginPath", handleBeginPath);
+    socket.on("drawLine", handleDrawLine);
+
     return () => {
         canvas.removeEventListener('mousedown', handleMouseDown)
         canvas.removeEventListener('mousemove', handleMouseMove)
         canvas.removeEventListener('mouseup', handleMouseUp)
+
+        socket.off("beginPath", handleBeginPath);//cleaning off the function
+        socket.off("drawLine", handleDrawLine);
     }
 
     }, [])//useEffect is just a hook not a lifecycle method
