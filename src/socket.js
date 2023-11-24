68372389d77f73a4efc2a1fe4be6560b7282de7f
REEKRAJROY <reekrajroy@gmail.com>
2023-11-24 13:03:57 +0530

    Adding BE endpoint

--- src/socket.js
+++ src/socket.js
@@ -1,3 +1,3 @@
 import { io } from "socket.io-client";
-
-export const socket = io('http://localhost:3500');
+const url = process.env.NODE_ENV === 'production' ? 'https://sketchbook-server-cwtl.onrender.com' : 'http://localhost:3500'
+export const socket = io(url);
